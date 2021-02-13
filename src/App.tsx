6e80e96cf696ce7f32ef2414696714e6f6641ba0
Ammar <ammarraneez@gmail.com>
2021-02-13 14:21:10 +0530

    created boiler template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import QuestionCard from './components/QuestionCard/QuestionCard';
 
 function App() {
+	const startTrivia = async () => {
+
+    }
+
+    const checkAnswer = (e : React.MouseEvent<HTMLButtonElement>) => {
+
+    }
+
+    const nextQuestion = () => {
+        
+    }
+
 	return (
 		<div className="app">
-			Quiz
+			<h1>Nice Quiz</h1>
+			<button className="start" onClick={startTrivia}>
+				Start
+			</button>
+			<p className="score">Score:</p>
+			<p>Loading Questions...</p>
+			<QuestionCard />
+			<button className="next" onClick={nextQuestion}>
+				Next Question
+			</button>
 		</div>
 	);
 }
